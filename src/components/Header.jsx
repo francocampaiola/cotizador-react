7ac31de2d00d6a91bd1554de8e929841b56e6bfe
Franco Campaiola <francocampaiola@hotmail.com>
2022-03-05 20:19:31 -0300

    :+1: Se incorpora la libreria de @emotion/styled al proyecto.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,12 @@
 import React from 'react'
+import styled from '@emotion/styled';
+
+const contenedorHeader = styled.header`
+  background-color: #26C6DA;
+  padding: 10px;
+  font-weight: bold;
+  color: #FFFFFF;
+`
 
 const Header = ({titulo}) => {
   return (
